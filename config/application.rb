6261b5a80a80fdb4efe0f1d7b359416782b0de6e
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-04-08 12:11:39 +0200

    Add host for training.decidim.org in Rails configuration

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module DecidimTraining
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.hosts << "training.decidim.org"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
