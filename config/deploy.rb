a7191762266128a7773ab816a5cf824f2b21e5b6
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-07-08 12:25:35 +0200

    Correct linked dirs and files in capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :branch, :main
 set :deploy_to, "/home/app-decidim_training/app"
 set :assets_manifests, nil
 
-# append :linked_files, "config/database.yml", 'config/master.key'
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
+append :linked_files, ".rbenv-vars"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads", "storage", "node_modules"
 
 Rake::Task["deploy:assets:backup_manifest"].clear_actions
