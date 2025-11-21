fc0f3485e981f6fd16295c93cb013c42f4261fe0
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-11-21 14:17:06 +0100

    Tune deploy config (rbenv-vars and sidekiq)

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,14 @@ set :branch, :main
 set :deploy_to, "/home/app-decidim_training/app"
 set :assets_manifests, nil
 
-append :linked_files, ".rbenv-vars"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads", "storage", "node_modules"
+
+after "deploy:finishing", "restart_sidekiq"
+
+task :restart_sidekiq do
+  on roles(:app), in: :sequence, wait: 5 do
+    execute "systemctl restart sidekiq-training"
+  end
+end
 
 Rake::Task["deploy:assets:backup_manifest"].clear_actions
